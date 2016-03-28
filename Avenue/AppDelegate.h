b766002eb30532b2383ae40da3feed35e672bac4
anafinotti <anafinotti@hotmail.com>
2016-03-27 21:42:03 -0300

    initial commit

---
+++ Avenue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Avenue
+//
+//  Created by Ana Finotti on 3/27/16.
+//  Copyright © 2016 Finotti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Avenue/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Avenue
+//
+//  Created by Ana Finotti on 3/27/16.
+//  Copyright © 2016 Finotti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
